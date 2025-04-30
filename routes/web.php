9757e058e72e7ad1d4e45256d1c5482121d2f65e
Samy <mohamed230100561@sut.edu.eg>
2025-04-30 18:24:02 +0300

    Adding front-end to Admin

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\backend\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/** 
+ *           ----------------
+ *            Admin Routes
+ *           ----------------
+ */
+Route::get('/Admin/login', [AdminController::class, 'login'])->name('admin.login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -17,4 +25,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
