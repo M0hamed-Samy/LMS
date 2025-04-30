5ff4332ac6e3ecb4b5329a5ca7222857de641539
M0hamed-Samy <mohamezzz700@gmail.com>
2025-04-30 21:39:50 +0300

    Admin full dashboard and signout functionality

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,34 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\backend\AdminController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\backend\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-/** 
- *           ----------------
- *            Admin Routes
- *           ----------------
- */
-Route::get('/Admin/login', [AdminController::class, 'login'])->name('admin.login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 
-Route::middleware(['auth', 'verified', 'role:admin'])->prefix('admin')->name('admin')->group(function () {
+
+/* Admin Route   */
+Route::get('/admin/login', [AdminController::class, 'login'])->name('admin.login');
+
+
+Route::middleware(['auth', 'verified', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+    Route::post('/logout', [AdminController::class, 'destroy'])
+        ->name('logout');
 });
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
